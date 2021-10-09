620a067fa7c368f72a7cac6e5e1544286e1cbbbe
Alexandre SAJUS <asajus47@gmail.com>
2021-10-09 02:36:30 +0200

    V1: functional game and DQN agent

---
+++ DQN/test_env_DQN.py
@@ -0,0 +1,36 @@
+"""
+2D Quadcopter AI by Alexandre Sajus
+
+More information at:
+https://github.com/AlexandreSajus/2D-Quadcopter-AI
+
+This is a helpful way to test env_DQN
+"""
+
+from DQN.env_DQN import droneEnv
+
+env = droneEnv(True, False)
+
+obs = env.reset()
+env.render("yes")
+
+print("Observation space:")
+print(env.observation_space)
+print("")
+print("Action space:")
+print(env.action_space)
+print("")
+print("Action space sample:")
+print(env.action_space.sample())
+
+# Choose an action to execute n_steps times
+action = 1
+n_steps = 20
+for step in range(n_steps):
+    print("Step {}".format(step + 1))
+    obs, reward, done, info = env.step(action)
+    print('obs=', obs, 'reward=', reward, 'done=', done)
+    env.render("yes")
+    if done:
+        print("Done!", "reward=", reward)
+        break
