c988179787f24514a985182a3011677b5872bd84
AlexandreSajus <asajus47@gmail.com>
2021-10-14 22:39:47 +0200

    Improved Human Game

--- Human/drone_game.py
+++ Human/drone_game.py
@@ -4,9 +4,9 @@
 More information at:
 https://github.com/AlexandreSajus/2D-Quadcopter-AI
 
-This is a simple game where you control a 2D Quadcopter with arrow keys
+This is angle simple game where you control angle 2D Quadcopter with arrow keys
 The goal is to reach as many targets as possible within the time limit
-This is used as a base to create a simulation environment
+This is used as angle base to create angle simulation environment
 """
 
 import pygame
@@ -26,7 +26,7 @@ gravity = 0.08
 thruster_amplitude = 0.04
 # Propeller force for LEFT and RIGHT rotations
 diff_amplitude = 0.003
-# By default, thruster will apply a force of thruster_mean
+# By default, thruster will apply angle force of thruster_mean
 thruster_mean = 0.04
 mass = 1
 # Length from center of mass to propeller
@@ -38,108 +38,171 @@ FramePerSec = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+# Loading player and target sprites
 player_width = 80
-player_speed = 0.3
+player_animation_speed = 0.3
 player = []
-for i in range(1,5):
-    image = pygame.image.load(os.path.join("assets/balloon-flat-asset-pack/png/objects/drone-sprites/drone-" + str(i) + ".png"))
+for i in range(1, 5):
+    image = pygame.image.load(os.path.join(
+        "assets/balloon-flat-asset-pack/png/objects/drone-sprites/drone-" + str(i) + ".png"))
     image.convert()
-    player.append(pygame.transform.scale(image,(player_width,int(player_width*0.30))))
+    player.append(pygame.transform.scale(
+        image, (player_width, int(player_width*0.30))))
 
 target_width = 30
-target_speed = 0.1
+target_animation_speed = 0.1
 target = []
-for i in range(1,8):
-    image = pygame.image.load(os.path.join("assets/balloon-flat-asset-pack/png/balloon-sprites/red-plain/red-plain-" + str(i) + ".png"))
+for i in range(1, 8):
+    image = pygame.image.load(os.path.join(
+        "assets/balloon-flat-asset-pack/png/balloon-sprites/red-plain/red-plain-" + str(i) + ".png"))
     image.convert()
-    target.append(pygame.transform.scale(image,(target_width,int(target_width*1.73))))
+    target.append(pygame.transform.scale(
+        image, (target_width, int(target_width*1.73))))
 
+# Loading background sprites
+cloud1 = pygame.image.load(os.path.join(
+    "assets/balloon-flat-asset-pack/png/background-elements/cloud-1.png"))
+cloud2 = pygame.image.load(os.path.join(
+    "assets/balloon-flat-asset-pack/png/background-elements/cloud-2.png"))
+sun = pygame.image.load(os.path.join(
+    "assets/balloon-flat-asset-pack/png/background-elements/sun.png"))
+cloud1.set_alpha(124)
+(x_cloud1, y_cloud1, speed_cloud1) = (150, 200, 0.3)
+cloud2.set_alpha(124)
+(x_cloud2, y_cloud2, speed_cloud2) = (400, 500, -0.2)
+sun.set_alpha(124)
+
+# Loading fonts
 pygame.font.init()
-myfont = pygame.font.Font('assets/fonts/Roboto-Regular.ttf', 30)
+info_font = pygame.font.Font('assets/fonts/Roboto-Regular.ttf', 30)
+respawn_font = pygame.font.Font('assets/fonts/Roboto-Bold.ttf', 90)
 
-# Initialize physics variables (a=angle) (xt,yt=target_coords)
-(a, ad, add) = (0, 0, 0)
-(x, xd, xdd) = (400, 0, 0)
-(y, yd, ydd) = (400, 0, 0)
-xt = randrange(200, 600)
-yt = randrange(200, 600)
+# Initialize physics variables (angle=angle) (x_target,y_target=target_coords)
+(angle, angular_speed, angular_acceleration) = (0, 0, 0)
+(x_position, x_speed, x_acceleration) = (400, 0, 0)
+(y_position, y_speed, y_acceleration) = (400, 0, 0)
+x_target = randrange(200, 600)
+y_target = randrange(200, 600)
 
 # Initialize game variables
 target_counter = 0
 time = 0
 step = 0
 time_limit = 30
+dead = False
+respawn_timer_max = 3
+respawn_timer = 3
 
 # Game loop
 while True:
     pygame.event.get()
-    screen.fill((131,176,181))
+
+    # Display background
+    screen.fill((131, 176, 181))
+
+    x_cloud1 += speed_cloud1
+    if x_cloud1 > WIDTH:
+        x_cloud1 = -cloud1.get_width()
+    screen.blit(cloud1, (x_cloud1, y_cloud1))
+
+    x_cloud2 += speed_cloud2
+    if x_cloud2 < -cloud2.get_width():
+        x_cloud2 = WIDTH
+    screen.blit(cloud2, (x_cloud2, y_cloud2))
+
+    screen.blit(sun, (630, -100))
 
     time += 1/60
     step += 1
 
-    # Initialize accelerations
-    xdd = 0
-    ydd = gravity
-    add = 0
+    if dead == False:
+        # Initialize accelerations
+        x_acceleration = 0
+        y_acceleration = gravity
+        angular_acceleration = 0
 
-    # Calculate propeller force in function of input
-    thruster_left = thruster_mean
-    thruster_right = thruster_mean
-    pressed_keys = pygame.key.get_pressed()
-    if pressed_keys[K_UP]:
-        thruster_left += thruster_amplitude
-        thruster_right += thruster_amplitude
-    if pressed_keys[K_DOWN]:
-        thruster_left -= thruster_amplitude
-        thruster_right -= thruster_amplitude
-    if pressed_keys[K_LEFT]:
-        thruster_left -= diff_amplitude
-    if pressed_keys[K_RIGHT]:
-        thruster_right -= diff_amplitude
+        # Calculate propeller force in function of input
+        thruster_left = thruster_mean
+        thruster_right = thruster_mean
+        pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[K_UP]:
+            thruster_left += thruster_amplitude
+            thruster_right += thruster_amplitude
+        if pressed_keys[K_DOWN]:
+            thruster_left -= thruster_amplitude
+            thruster_right -= thruster_amplitude
+        if pressed_keys[K_LEFT]:
+            thruster_left -= diff_amplitude
+        if pressed_keys[K_RIGHT]:
+            thruster_right -= diff_amplitude
 
-    # Calculate accelerations according to Newton's laws of motion
-    xdd += -(thruster_left + thruster_right)*sin(a*pi/180)/mass
-    ydd += -(thruster_left + thruster_right)*cos(a*pi/180)/mass
-    add += arm*(thruster_right - thruster_left)/mass
+        # Calculate accelerations according to Newton's laws of motion
+        x_acceleration += -(thruster_left + thruster_right) * \
+            sin(angle*pi/180)/mass
+        y_acceleration += -(thruster_left + thruster_right) * \
+            cos(angle*pi/180)/mass
+        angular_acceleration += arm*(thruster_right - thruster_left)/mass
 
-    # Calculate speed
-    xd += xdd
-    yd += ydd
-    ad += add
+        # Calculate speed
+        x_speed += x_acceleration
+        y_speed += y_acceleration
+        angular_speed += angular_acceleration
 
-    # Calculate position
-    x += xd
-    y += yd
-    a += ad
+        # Calculate position
+        x_position += x_speed
+        y_position += y_speed
+        angle += angular_speed
 
-    # Calculate distance to target
-    dist = sqrt((x - xt)**2 + (y - yt)**2)
-    # If target reached, respawn target
-    if dist < 50:
-        xt = randrange(200, 600)
-        yt = randrange(200, 600)
-        target_counter += 1
+        # Calculate distance to target
+        dist = sqrt((x_position - x_target)**2 + (y_position - y_target)**2)
 
-    # Failure conditions
-    if dist > 1000 or time > time_limit:
+        # If target reached, respawn target
+        if dist < 50:
+            x_target = randrange(200, 600)
+            y_target = randrange(200, 600)
+            target_counter += 1
+
+        # If to far, die and respawn after timer
+        elif dist > 1000:
+            dead = True
+            respawn_timer = respawn_timer_max
+    else:
+        # Display respawn timer
+        respawn_text = respawn_font.render(
+            str(int(respawn_timer)+1), True, (255, 255, 255))
+        respawn_text.set_alpha(124)
+        screen.blit(respawn_text, (WIDTH/2 - respawn_text.get_width() /
+                    2, HEIGHT/2 - respawn_text.get_height()/2))
+
+        respawn_timer -= 1/60
+        # Respawn
+        if respawn_timer < 0:
+            dead = False
+            (angle, angular_speed, angular_acceleration) = (0, 0, 0)
+            (x_position, x_speed, x_acceleration) = (400, 0, 0)
+            (y_position, y_speed, y_acceleration) = (400, 0, 0)
+
+    # Ending conditions
+    if time > time_limit:
         break
 
-    target_sprite = target[int(step*target_speed)%len(target)]
-    screen.blit(target_sprite, (xt - int(target_sprite.get_width()/2),
-                yt - int(target_sprite.get_height()/2)))
-    player_sprite = player[int(step*player_speed)%len(player)]
-    player_copy = pygame.transform.rotate(player_sprite, a)
-    screen.blit(player_copy, (x - int(player_copy.get_width()/2),
-                y - int(player_copy.get_height()/2)))
+    # Display target and player
+    target_sprite = target[int(step*target_animation_speed) % len(target)]
+    screen.blit(target_sprite, (x_target - int(target_sprite.get_width()/2),
+                y_target - int(target_sprite.get_height()/2)))
 
-    # Update Pygame screen
-    textsurface = myfont.render(
+    player_sprite = player[int(step*player_animation_speed) % len(player)]
+    player_copy = pygame.transform.rotate(player_sprite, angle)
+    screen.blit(player_copy, (x_position - int(player_copy.get_width()/2),
+                y_position - int(player_copy.get_height()/2)))
+
+    # Update text
+    target_target = info_font.render(
         'Collected : ' + str(target_counter), True, (255, 255, 255))
-    screen.blit(textsurface, (20, 20))
-    textsurface3 = myfont.render(
+    screen.blit(target_target, (20, 20))
+    time_text = info_font.render(
         'Time : ' + str(int(time_limit - time)), True, (255, 255, 255))
-    screen.blit(textsurface3, (20, 60))
+    screen.blit(time_text, (20, 60))
 
     pygame.display.update()
     FramePerSec.tick(FPS)
