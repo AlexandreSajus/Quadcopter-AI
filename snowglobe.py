3310b6babead3afc5825989be01bbf8c9d8fac49
AlexandreSajus <asajus47@gmail.com>
2021-12-09 23:36:30 +0100

    :hammer: huge refactor

--- snowglobe.py
+++ snowglobe.py
@@ -7,21 +7,52 @@ https://github.com/AlexandreSajus/2D-Quadcopter-AI
 Christmas themed mod to the main game
 """
 
-import numpy as np
+import random
 import pygame
 import os
 from pygame.locals import *
 from math import sin, cos, pi, sqrt, atan2
 from random import randrange
-from player import HumanPlayer, PIDPlayer, DQNPlayer
 
-# Game constants
+from pygame.math import disable_swizzling
+from player import PIDPlayer
+
+"""
+Game variables
+"""
+# FPS, w and h of the window
 FPS = 60
 WIDTH = 800
 HEIGHT = 800
 
+# simulation step number
+step = 0
+# snowglobe geometry
 snowglobe_radius = 350
+snowglobe_edge = 20
+# radius of the snow particles
+(snow_min_radius, snow_max_radius) = (2, 8)
+# number of snow particles
+n_particles = 2000
+# factor to define collision player-snowglobe
+player_collision_margin = 0.9
+# factor to define collision player-snow
+particle_collision_margin = 0.95
+# drag coefficient for snow
+drag = 0.2
+# snow-drone interaction force
+interaction_force = 0.3
+# snow-drone interaction distance
+interaction_distance = 200
+# snow-drone interaction cone angle
+interaction_angle = 30
+# random snow speed
+random_snow_speed = 0.4
 
+
+"""
+Drone constants
+"""
 # Physics constants
 gravity = 0.08
 # Propeller force for UP and DOWN
@@ -30,10 +61,15 @@ thruster_amplitude = 0.04
 diff_amplitude = 0.003
 # By default, thruster will apply angle force of thruster_mean
 thruster_mean = 0.04
+# Mass
 mass = 1
 # Length from center of mass to propeller
 arm = 25
 
+
+"""
+Init Pygame and sprites
+"""
 # Initialize Pygame, load sprites
 FramePerSec = pygame.time.Clock()
 
@@ -45,36 +81,46 @@ player_width = 80
 player_animation_speed = 0.3
 player_animation = []
 for i in range(1, 5):
-    image = pygame.image.load(os.path.join(
-        "assets/balloon-flat-asset-pack/png/objects/drone-sprites/drone-" + str(i) + ".png"))
+    image = pygame.image.load(
+        os.path.join(
+            "assets/balloon-flat-asset-pack/png/objects/drone-sprites/drone-"
+            + str(i)
+            + ".png"
+        )
+    )
     image.convert()
-    player_animation.append(pygame.transform.scale(
-        image, (player_width, int(player_width*0.30))))
+    player_animation.append(
+        pygame.transform.scale(image, (player_width, int(player_width * 0.30)))
+    )
 
 # Loading background sprites
-cloud1 = pygame.image.load(os.path.join(
-    "assets/balloon-flat-asset-pack/png/background-elements/cloud-1.png"))
-cloud2 = pygame.image.load(os.path.join(
-    "assets/balloon-flat-asset-pack/png/background-elements/cloud-2.png"))
-sun = pygame.image.load(os.path.join(
-    "assets/balloon-flat-asset-pack/png/background-elements/sun.png"))
+cloud1 = pygame.image.load(
+    os.path.join("assets/balloon-flat-asset-pack/png/background-elements/cloud-1.png")
+)
+cloud2 = pygame.image.load(
+    os.path.join("assets/balloon-flat-asset-pack/png/background-elements/cloud-2.png")
+)
+sun = pygame.image.load(
+    os.path.join("assets/balloon-flat-asset-pack/png/background-elements/sun.png")
+)
 cloud1.set_alpha(124)
 (x_cloud1, y_cloud1, speed_cloud1) = (150, 200, 0.3)
 cloud2.set_alpha(124)
 (x_cloud2, y_cloud2, speed_cloud2) = (400, 500, -0.2)
 sun.set_alpha(124)
 
-
-# Initialize game variables
-time = 0
-step = 0
-time_limit = 30
-respawn_timer_max = 3
-
+"""
+Init drone and snow particles
+"""
+# Create player
 player = PIDPlayer()
 
 
 class SnowParticle:
+    """
+    A snow particle
+    """
+
     def __init__(self, x, y, radius):
         self.x_position = x
         self.y_position = y
@@ -85,25 +131,57 @@ class SnowParticle:
         self.radius = radius
 
 
+# Create snow particles
 snow_particles = []
-n_particles = 2000
 for i in range(0, n_particles):
-    snow_particles.append(SnowParticle(
-        randrange(150, WIDTH - 150), randrange(150, HEIGHT-150), randrange(1, 3)))
+    while True:
+        x = randrange(0, WIDTH)
+        y = randrange(0, HEIGHT)
+        if sqrt((x - WIDTH / 2) ** 2 + (y - HEIGHT / 2) ** 2) < snowglobe_radius:
+            break
+    r = randrange(snow_min_radius, snow_max_radius)
+    snow_particles.append(SnowParticle(x, y, r))
 
-boost = False
+"""
+Utils
+"""
+
+
+def convert_to_circular(x, y, x_pos, y_pos):
+    """
+    Convert cartesian coordinates to circular
+    Returns r and theta coordinates
+    """
+    # Because pygame y-axis is inverted, y = -y
+    angle = atan2(HEIGHT / 2 - y_pos, x_pos - WIDTH / 2)
+    y = -y
+    r = x * cos(angle) + y * sin(angle)
+    theta = -sin(angle) * x + cos(angle) * y
+    return r, theta
+
+
+def convert_to_cartesian(r, theta, x_pos, y_pos):
+    """
+    Convert circular coordinates to cartesian
+    Returns x and y coordinates
+    """
+    # Because pygame y-axis is inverted, y = -y
+    angle = atan2(HEIGHT / 2 - y_pos, x_pos - WIDTH / 2)
+    x = r * cos(angle) - theta * sin(angle)
+    y = r * sin(angle) + theta * cos(angle)
+    y = -y
+    return x, y
 
 
 # Game loop
 while True:
     pygame.event.get()
 
-    boost = False
-    # If spacebar is pressed, boost
-    if pygame.key.get_pressed()[K_SPACE]:
-        boost = True
+    step += 1
 
-    # Display background
+    """
+    Background
+    """
     screen.fill((131, 176, 181))
 
     x_cloud1 += speed_cloud1
@@ -118,9 +196,18 @@ while True:
 
     screen.blit(sun, (630, -100))
 
-    time += 1/60
-    step += 1
+    # Snowglobe
+    pygame.draw.circle(
+        screen,
+        (200, 200, 200),
+        (int(WIDTH / 2), int(HEIGHT / 2)),
+        snowglobe_radius,
+        snowglobe_edge,
+    )
 
+    """
+    Player
+    """
     # Initialize accelerations
     player.x_acceleration = 0
     player.y_acceleration = gravity
@@ -132,41 +219,44 @@ while True:
 
     # Calculate propeller force in function of input
     thruster_left, thruster_right = player.act(
-        [target_x - player.x_position, player.x_speed, target_y - player.y_position, player.y_speed, player.angle, player.angular_speed])
+        [
+            target_x - player.x_position,
+            player.x_speed,
+            target_y - player.y_position,
+            player.y_speed,
+            player.angle,
+            player.angular_speed,
+        ]
+    )
 
     # Calculate accelerations according to Newton's laws of motion
-    force_x = -(thruster_left + thruster_right) * \
-        sin(player.angle*pi/180)/mass
-    force_y = -(thruster_left + thruster_right) * \
-        cos(player.angle*pi/180)/mass
-
-    position = [player.x_position, player.y_position]
-    force = [force_x, force_y]
-
-    distance_to_center = sqrt((position[0] - WIDTH/2)**2 +
-                              (position[1] - HEIGHT/2)**2)
+    force_x = -(thruster_left + thruster_right) * sin(player.angle * pi / 180) / mass
+    force_y = -(thruster_left + thruster_right) * cos(player.angle * pi / 180) / mass
 
     # Update accelerations
-    player.x_acceleration += force[0]
-    player.y_acceleration += force[1]
+    player.x_acceleration += force_x
+    player.y_acceleration += force_y
 
-    player.angular_acceleration += arm * \
-        (thruster_right - thruster_left)/mass
+    player.angular_acceleration += arm * (thruster_right - thruster_left) / mass
 
     # Calculate speed
     player.x_speed += player.x_acceleration
     player.y_speed += player.y_acceleration
     player.angular_speed += player.angular_acceleration
 
-    angle = atan2(position[1] - HEIGHT/2, position[0] - WIDTH/2)
-    r_speed = player.x_speed * cos(angle) + player.y_speed * sin(angle)
+    # Boundary conditions
+    distance_to_center = sqrt(
+        (player.x_position - WIDTH / 2) ** 2 + (player.y_position - HEIGHT / 2) ** 2
+    )
+    r_speed, theta_speed = convert_to_circular(
+        player.x_speed, player.y_speed, player.x_position, player.y_position
+    )
 
-    if distance_to_center > snowglobe_radius*0.9 and r_speed > 0:
-        theta_speed = player.x_speed * \
-            sin(angle) - player.y_speed * cos(angle)
-        r_speed = - r_speed*0.8
-        x_speed = r_speed * cos(angle) + theta_speed * sin(angle)
-        y_speed = r_speed * sin(angle) - theta_speed * cos(angle)
+    if distance_to_center > snowglobe_radius * player_collision_margin and r_speed > 0:
+        r_speed = -r_speed
+        x_speed, y_speed = convert_to_cartesian(
+            r_speed, theta_speed, player.x_position, player.y_position
+        )
         player.x_speed = x_speed
         player.y_speed = y_speed
 
@@ -175,60 +265,127 @@ while True:
     player.y_position += player.y_speed
     player.angle += player.angular_speed
 
-    player_sprite = player_animation[int(
-        step*player_animation_speed) % len(player_animation)]
+    # Animation
+    player_sprite = player_animation[
+        int(step * player_animation_speed) % len(player_animation)
+    ]
     player_copy = pygame.transform.rotate(player_sprite, player.angle)
     player_copy.set_alpha(255)
-    screen.blit(player_copy, (player.x_position - int(player_copy.get_width()/2),
-                player.y_position - int(player_copy.get_height()/2)))
+    screen.blit(
+        player_copy,
+        (
+            player.x_position - int(player_copy.get_width() / 2),
+            player.y_position - int(player_copy.get_height() / 2),
+        ),
+    )
 
-    # Draw a ring in the center of the screen
-    pygame.draw.circle(screen, (200, 200, 200),
-                       (int(WIDTH/2), int(HEIGHT/2)), snowglobe_radius, 5)
-
+    """
+    Snow particles
+    """
     for snow_particle in snow_particles:
-        snow_particle.x_acceleration = 0
-        snow_particle.y_acceleration = gravity*0.2
+        # Accelerations
+        snow_particle.x_acceleration = 0 - drag * snow_particle.x_speed
+        snow_particle.y_acceleration = gravity - drag * snow_particle.y_speed
 
-        distance_to_player = sqrt((snow_particle.x_position - player.x_position)**2 +
-                                  (snow_particle.y_position - player.y_position)**2)
+        # Drone interaction
+        distance_to_player = sqrt(
+            (snow_particle.y_position - player.y_position) ** 2
+            + (snow_particle.x_position - player.x_position) ** 2
+        )
+        angle_to_player = atan2(
+            snow_particle.y_position - player.y_position,
+            player.x_position - snow_particle.x_position,
+        )
+        angle_to_player = angle_to_player * 180 / pi
 
-        if distance_to_player < 100:
-            snow_particle.x_acceleration += 2*(
-                snow_particle.x_position - player.x_position)/distance_to_player
-            snow_particle.y_acceleration += 2*(
-                snow_particle.y_position - player.y_position)/distance_to_player
+        if distance_to_player < interaction_distance:
+            # If snow below the player, snow is attracted to the player
+            if interaction_angle < angle_to_player < 180 - interaction_angle:
+                snow_particle.x_acceleration -= (
+                    interaction_force
+                    * (snow_particle.x_position - player.x_position)
+                    / distance_to_player
+                )
+                snow_particle.y_acceleration -= (
+                    interaction_force
+                    * (snow_particle.y_position - player.y_position)
+                    / distance_to_player
+                )
+            # If snow above the player, snow is repelled from the player
+            elif -interaction_angle > angle_to_player > -180 + interaction_angle:
+                snow_particle.x_acceleration += (
+                    interaction_force
+                    * (snow_particle.x_position - player.x_position)
+                    / distance_to_player
+                )
+                snow_particle.y_acceleration += (
+                    interaction_force
+                    * (snow_particle.y_position - player.y_position)
+                    / distance_to_player
+                )
 
-        snow_particle.x_speed += snow_particle.x_acceleration - 0.02*snow_particle.x_speed
-        snow_particle.y_speed += snow_particle.y_acceleration - 0.02*snow_particle.y_speed
+        # Calculate speed
+        snow_particle.x_speed += snow_particle.x_acceleration
+        snow_particle.y_speed += snow_particle.y_acceleration
 
-        position = [snow_particle.x_position, snow_particle.y_position]
-        distance_to_center = sqrt((position[0] - WIDTH/2)**2 +
-                                  (position[1] - HEIGHT/2)**2)
+        # Add random noise to speed
+        snow_particle.x_speed += random.uniform(-random_snow_speed, random_snow_speed)
+        snow_particle.y_speed += random.uniform(-random_snow_speed, random_snow_speed)
 
-        angle = atan2(position[1] - HEIGHT/2, position[0] - WIDTH/2)
-        r_speed = snow_particle.x_speed * \
-            cos(angle) + snow_particle.y_speed * sin(angle)
-        theta_speed = snow_particle.x_speed * \
-            sin(angle) - snow_particle.y_speed * cos(angle)
-        theta_position = snow_particle.x_position * \
-            sin(angle) - snow_particle.y_position * cos(angle)
+        # Boundary conditions
+        distance_to_center = sqrt(
+            (snow_particle.x_position - WIDTH / 2) ** 2
+            + (snow_particle.y_position - HEIGHT / 2) ** 2
+        )
+        r_speed, theta_speed = convert_to_circular(
+            snow_particle.x_speed,
+            snow_particle.y_speed,
+            snow_particle.x_position,
+            snow_particle.y_position,
+        )
 
-        if distance_to_center > snowglobe_radius*1 and r_speed > 0:
-            r_speed = - r_speed*0.7
-            theta_speed = theta_speed
-            x_speed = r_speed * cos(angle) + theta_speed * sin(angle)
-            y_speed = r_speed * sin(angle) - theta_speed * cos(angle)
+        if (
+            distance_to_center > snowglobe_radius * particle_collision_margin
+            and r_speed > 0
+        ):
+            r_speed = -r_speed
+            x_speed, y_speed = convert_to_cartesian(
+                r_speed, theta_speed, snow_particle.x_position, snow_particle.y_position
+            )
             snow_particle.x_speed = x_speed
             snow_particle.y_speed = y_speed
 
-        if boost:
-            snow_particle.y_speed -= 1
+        # Calculate position
         snow_particle.x_position += snow_particle.x_speed
         snow_particle.y_position += snow_particle.y_speed
 
-        pygame.draw.circle(screen, (255, 255, 255), (int(snow_particle.x_position), int(
-            snow_particle.y_position)), snow_particle.radius, 0)
+        # Clamp position
+        r_position, theta_position = convert_to_circular(
+            snow_particle.x_position - WIDTH / 2,
+            snow_particle.y_position - HEIGHT / 2,
+            snow_particle.x_position,
+            snow_particle.y_position,
+        )
+
+        if r_position > snowglobe_radius * particle_collision_margin:
+            r_position = snowglobe_radius * particle_collision_margin
+            x_position, y_position = convert_to_cartesian(
+                r_position,
+                theta_position,
+                snow_particle.x_position,
+                snow_particle.y_position,
+            )
+            snow_particle.x_position = x_position + WIDTH / 2
+            snow_particle.y_position = y_position + HEIGHT / 2
+
+        # Animation
+        pygame.draw.circle(
+            screen,
+            (255, 255, 255),
+            (int(snow_particle.x_position), int(snow_particle.y_position)),
+            snow_particle.radius,
+            0,
+        )
 
     pygame.display.update()
     FramePerSec.tick(FPS)
