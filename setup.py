621547115f26f23c55e51986a4998663f3a8e36c
Alexandre Sajus <alexandre.sajus@student-cs.fr>
2022-11-04 11:17:24 +0100

    :bug: Adds requirements to setup

--- setup.py
+++ setup.py
@@ -2,4 +2,11 @@
 
 from setuptools import setup
 
-setup()
+
+def get_requirements():
+    """Load requirements from file."""
+    requirements_file = open("requirements.txt")
+    return requirements_file.readlines()
+
+
+setup(install_requires=get_requirements())
